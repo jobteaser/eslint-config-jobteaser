64636cfd642f6fcf540d1faed7db702d366795a7
adrian-amigues <adrian.amigues@gmail.com>
2019-03-11 14:24:16 +0100

    style(global): remove spaces inside square brackets

--- rules/base.js
+++ rules/base.js
@@ -1,23 +1,23 @@
 module.exports = {
   rules: {
-    "arrow-body-style": [ "error", "as-needed" ],
-    "array-bracket-spacing": [ "error", "never" ],
-    "brace-style": [ "error", "1tbs" ],
-    "class-methods-use-this": [ "off" ],
-    "comma-dangle": [ "error", "always-multiline" ],
-    "comma-style": [ "error", "last" ],
-    "computed-property-spacing": [ "error", "never" ],
-    "consistent-return": [ "warn" ],
-    "curly": [ "error" ],
-    "dot-location": [ "error", "property" ],
-    "guard-for-in": [ "warn" ],
-    "id-length": [ "error",
+    "arrow-body-style": ["error", "as-needed"],
+    "array-bracket-spacing": ["error", "never"],
+    "brace-style": ["error", "1tbs"],
+    "class-methods-use-this": ["off"],
+    "comma-dangle": ["error", "always-multiline"],
+    "comma-style": ["error", "last"],
+    "computed-property-spacing": ["error", "never"],
+    "consistent-return": ["warn"],
+    "curly": ["error"],
+    "dot-location": ["error", "property"],
+    "guard-for-in": ["warn"],
+    "id-length": ["error",
       {
         "min": 3,
-        "exceptions": [ "e", "id", "cx" ]
+        "exceptions": ["e", "id", "cx"]
       }
     ],
-    "max-len": [ "warn",
+    "max-len": ["warn",
       {
         "code": 120,
         "tabWidth": 2,
@@ -28,48 +28,48 @@ module.exports = {
         "ignoreTemplateLiterals": true
       }
     ],
-    "no-bitwise": [ "error" ],
-    "no-console": [ "error",
+    "no-bitwise": ["error"],
+    "no-console": ["error",
       {
-        "allow": [ "info", "warn", "error" ]
+        "allow": ["info", "warn", "error"]
       }
     ],
-    "no-multiple-empty-lines": [ "error",
+    "no-multiple-empty-lines": ["error",
       {
         "max": 2
       }
     ],
-    "no-nested-ternary": [ "warn" ],
-    "no-plusplus": [ "error",
+    "no-nested-ternary": ["warn"],
+    "no-plusplus": ["error",
       {
         "allowForLoopAfterthoughts": true
       }
     ],
-    "no-prototype-builtins": [ "off" ],
-    "no-trailing-spaces": [ "warn",
+    "no-prototype-builtins": ["off"],
+    "no-trailing-spaces": ["warn",
       {
         "skipBlankLines": true,
         "ignoreComments": true
       }
     ],
-    "no-var": [ "error" ],
-    "object-curly-spacing": [ "error", "always" ],
-    "one-var": [ "error", "never" ],
-    "operator-assignment": [ "error", "never" ],
-    "prefer-const": [ "error" ],
-    "quotes": [ "error", "double" ],
+    "no-var": ["error"],
+    "object-curly-spacing": ["error", "always"],
+    "one-var": ["error", "never"],
+    "operator-assignment": ["error", "never"],
+    "prefer-const": ["error"],
+    "quotes": ["error", "double"],
 
-    "semi": [ "error", "never" ],
-    "space-before-blocks": [ "error", "always" ],
-    "space-before-function-paren": [ "off", "never" ],
-    "spaced-comment": [ "error", "always" ],
-    "space-in-parens": [ "error", "never" ],
-    "space-unary-ops": [ "error",
+    "semi": ["error", "never"],
+    "space-before-blocks": ["error", "always"],
+    "space-before-function-paren": ["off", "never"],
+    "spaced-comment": ["error", "always"],
+    "space-in-parens": ["error", "never"],
+    "space-unary-ops": ["error",
       {
         "words": true,
         "nonwords": false
       }
     ],
-    "prefer-destructuring": [ "error", { "object": true, "array": false } ],
+    "prefer-destructuring": ["error", { "object": true, "array": false }],
   }
 }
--- rules/jsx-ally.js
+++ rules/jsx-ally.js
@@ -5,26 +5,26 @@ module.exports = {
     },
   },
   rules: {
-    "jsx-a11y/alt-text": [ "off" ],
-    "jsx-a11y/anchor-has-content": [ "off" ],
-    "jsx-a11y/href-no-hash": [ "off" ],
-    "jsx-a11y/iframe-has-title": [ "off" ],
-    "jsx-a11y/label-has-for": [ "error",
+    "jsx-a11y/alt-text": ["off"],
+    "jsx-a11y/anchor-has-content": ["off"],
+    "jsx-a11y/href-no-hash": ["off"],
+    "jsx-a11y/iframe-has-title": ["off"],
+    "jsx-a11y/label-has-for": ["error",
       {
-        "components": [ "label" ],
+        "components": ["label"],
         "required": {
-          "every": [ "nesting", "id" ]
+          "every": ["nesting", "id"]
         },
         "allowChildren": true
       }
     ],
-    "jsx-a11y/media-has-caption": [ "off" ],
-    "jsx-a11y/no-noninteractive-element-interactions": [ "off" ],
-    "jsx-a11y/no-noninteractive-tabindex": [ "off" ],
-    "jsx-a11y/no-static-element-interactions": [ "off" ],
-    "jsx-quotes": [ "error", "prefer-double" ],
-    "jsx-a11y/click-events-have-key-events": [ "off" ],
-    "jsx-a11y/anchor-is-valid": [ "off" ],
-    "jsx-a11y/label-has-associated-control": [ "warn" ],
+    "jsx-a11y/media-has-caption": ["off"],
+    "jsx-a11y/no-noninteractive-element-interactions": ["off"],
+    "jsx-a11y/no-noninteractive-tabindex": ["off"],
+    "jsx-a11y/no-static-element-interactions": ["off"],
+    "jsx-quotes": ["error", "prefer-double"],
+    "jsx-a11y/click-events-have-key-events": ["off"],
+    "jsx-a11y/anchor-is-valid": ["off"],
+    "jsx-a11y/label-has-associated-control": ["warn"],
   }
 }
