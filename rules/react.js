195d794458093e65f36b282b2405288e5447030c
Fabien Raynaud <fabien.raynaud@jobteaser.com>
2019-11-14 18:09:55 +0100

    feat(react): add new rules (#21)

--- rules/react.js
+++ rules/react.js
@@ -8,13 +8,14 @@ module.exports = {
     },
   },
   rules: {
-    "react/forbid-prop-types": ["off"],
+    "react/forbid-prop-types": ["error"],
     "react/jsx-closing-bracket-location": ["error", "tag-aligned"],
     "react/jsx-curly-spacing": ["error", "never"],
     "react/jsx-filename-extension": ["off"],
     "react/jsx-indent-props": ["error", 2],
     "react/jsx-no-duplicate-props": ["error"],
     "react/jsx-no-literals": ["off"],
+    "react/no-typos": ["error"],
     "react/jsx-no-undef": ["error"],
     "react/jsx-uses-react": ["error"],
     "react/jsx-uses-vars": ["error"],
@@ -41,7 +42,7 @@ module.exports = {
     "react/self-closing-comp": ["error"],
     "react/sort-comp": ["off"],
     "react/prefer-stateless-function": ["off"],
-    "react/destructuring-assignment": ["off"],
+    "react/destructuring-assignment": ["error"],
     "react/no-access-state-in-setstate": ["off"],
   }
 }
