c96fc64ff327b087e7b9d62208c8b10f0ff2b82a
Fabien Raynaud <fabien.raynaud@jobteaser.com>
2019-11-25 16:05:39 +0100

    feat(import): enforce named import/export and avoid default export

--- rules/import.js
+++ rules/import.js
@@ -2,8 +2,10 @@ module.exports = {
   rules: {
     "import/extensions": ["off"],
     "import/first": ["warn"],
+    "import/named": ["error"],
+    "import/no-default-export": ["error"],
+    "import/no-extraneous-dependencies": ["off"],
     "import/no-unresolved": ["off"],
     "import/prefer-default-export": ["off"],
-    "import/no-extraneous-dependencies": ["off"],
   }
 }
