aa4f93c09535797ee6948e74b5da7a2e6af2a0b9
Fabien Raynaud <fabien.raynaud@jobteaser.com>
2019-11-26 14:36:38 +0100

    feat(import): enforce named import/export and avoid default export (#24)
    * feat(import): enforce named import/export and avoid default export
    
    * feat(import): forbid parent relative path

--- rules/import.js
+++ rules/import.js
@@ -2,8 +2,11 @@ module.exports = {
   rules: {
     "import/extensions": ["off"],
     "import/first": ["warn"],
+    "import/named": ["error"],
+    "import/no-default-export": ["error"],
+    "import/no-extraneous-dependencies": ["off"],
+    "import/no-relative-parent-imports": ["error"],
     "import/no-unresolved": ["off"],
     "import/prefer-default-export": ["off"],
-    "import/no-extraneous-dependencies": ["off"],
   }
 }
