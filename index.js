b6538a64e11de90c1da89ec686a500cb79372b60
alex00 <alexandre.francois0000@gmail.com>
2022-10-27 16:55:35 +0200

    chore(global): update to version 8.26.0 WIP
    https://jobteaser.atlassian.net/browse/OP-2062

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 module.exports = {
-  parser: "babel-eslint",
+  // parser: "babel-eslint",
   extends: [
     "eslint-config-airbnb",
-    "./rules/base",
-    "./rules/import",
-    "./rules/jsx-ally",
-    "./rules/lodash",
-    "./rules/react",
-    "./rules/react-hooks",
-    "eslint-config-prettier",
-    "eslint-config-prettier/react"
   ].map(require.resolve),
   plugins: [
-    "eslint-plugin-import",
-    "eslint-plugin-jsx-a11y",
-    "eslint-plugin-lodash",
-    "eslint-plugin-react",
-    "eslint-plugin-react-hooks"
   ],
   rules: {}
 };
