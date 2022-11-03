b62ef4af50b85e849c7a0be6c36a750285802f52
Clément Gateaud <clement.gateaud@gmail.com>
2022-11-03 11:30:02 +0100

    chore(shared): add testing library and jest dom eslint

--- index.js
+++ index.js
@@ -4,19 +4,29 @@ module.exports = {
     "eslint-config-airbnb",
     "./rules/base",
     "./rules/import",
+    "./rules/jest-dom",
     "./rules/jsx-ally",
     "./rules/lodash",
     "./rules/react",
     "./rules/react-hooks",
+    ".rules/testing-library",
     "eslint-config-prettier",
-    "eslint-config-prettier/react"
+    "eslint-config-prettier/react",
   ].map(require.resolve),
   plugins: [
     "eslint-plugin-import",
+    "eslint-plugin-jest-dom",
     "eslint-plugin-jsx-a11y",
     "eslint-plugin-lodash",
     "eslint-plugin-react",
-    "eslint-plugin-react-hooks"
+    "eslint-plugin-react-hooks",
+    "eslint-plugin-testing-library",
   ],
-  rules: {}
+  rules: {},
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: [".rules/jest-dom", ".rules/testing-library"],
+    },
+  ],
 };
---
+++ rules/jest-dom.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: ["jest-dom"],
+  rules: {
+    "jest-dom/prefer-checked": "error",
+    "jest-dom/prefer-empty": "error",
+    "jest-dom/prefer-enabled-disabled": "error",
+    "jest-dom/prefer-focus": "error",
+    "jest-dom/prefer-in-document": "error",
+    "jest-dom/prefer-required": "error",
+    "jest-dom/prefer-to-have-attribute": "error",
+    "jest-dom/prefer-to-have-class": "error",
+    "jest-dom/prefer-to-have-style": "error",
+    "jest-dom/prefer-to-have-text-content": "error",
+    "jest-dom/prefer-to-have-value": "error",
+  },
+};
