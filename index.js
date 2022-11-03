14073320ec021f2ad12d93faab8a102bf544f690
Clément Gateaud <clement.gateaud@gmail.com>
2022-11-03 11:30:02 +0100

    chore(shared): add testing library and jest dom eslint

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ module.exports = {
     "./rules/lodash",
     "./rules/react",
     "./rules/react-hooks",
-    "eslint-config-prettier"
+    "eslint-config-prettier",
   ].map(require.resolve),
   plugins: [
     "eslint-plugin-import",
     "eslint-plugin-jsx-a11y",
     "eslint-plugin-lodash",
     "eslint-plugin-react",
-    "eslint-plugin-react-hooks"
+    "eslint-plugin-react-hooks",
   ],
-  rules: {}
+  rules: {},
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["./rules/jest-dom", "./rules/testing-library"],
+    },
+  ],
 };
