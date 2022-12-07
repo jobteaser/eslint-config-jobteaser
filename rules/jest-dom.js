43491d5ba25ebb0850ad00356fb57f99d7d8da0e
Clément Gateaud <clement.gateaud@jobteaser.com>
2022-12-07 17:05:10 +0100

    chore(shared): add testing library and jest dom eslint (#45)
    * chore(shared): add testing library and jest dom eslint
    
    * change some rules

---
+++ rules/jest-dom.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: ["jest-dom"],
+  rules: {
+    "jest-dom/prefer-checked": "error",
+    "jest-dom/prefer-empty": "warn",
+    "jest-dom/prefer-enabled-disabled": "error",
+    "jest-dom/prefer-focus": "error",
+    "jest-dom/prefer-in-document": "warn",
+    "jest-dom/prefer-required": "error",
+    "jest-dom/prefer-to-have-attribute": "warn",
+    "jest-dom/prefer-to-have-class": "error",
+    "jest-dom/prefer-to-have-style": "error",
+    "jest-dom/prefer-to-have-text-content": "warn",
+    "jest-dom/prefer-to-have-value": "error"
+  },
+};
